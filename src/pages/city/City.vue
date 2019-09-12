d0482d6323008874c760fd132c323ae041db55f8
picongzhi <picongzhi@gmail.com>
2019-09-13 01:23:36 +0800

    feat(city): add city router and header

---
+++ src/pages/city/City.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <city-header>city</city-header>
+  </div>
+</template>
+
+<script>
+import CityHeader from './components/Header'
+
+export default {
+  name: 'CityHome',
+  components: {
+    CityHeader
+  }
+}
+</script>
+
+<style lang='stylus' scoped>
+</style>
